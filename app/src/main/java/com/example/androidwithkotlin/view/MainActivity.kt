cdb5ef70525cb2b5844211f190598ed8af922ff2
awesome66613 <nechepurnov.m@gmail.com>
2021-04-22 18:49:48 +0300

    HomeWork9

--- app/src/main/java/com/example/androidwithkotlin/view/MainActivity.kt
+++ app/src/main/java/com/example/androidwithkotlin/view/MainActivity.kt
@@ -1,19 +1,62 @@
 package com.example.androidwithkotlin.view
 
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import com.example.androidwithkotlin.R
+import com.example.androidonkotlin.R
 import com.example.androidwithkotlin.view.main.MainFragment
+import com.example.androidwithkotlin.view.contacts.ContentProviderFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private val receiver = ConnectivityBroadcastReceiver()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitAllowingStateLoss()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitAllowingStateLoss()
         }
+        registerReceiver(receiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_screen_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_history -> {
+                supportFragmentManager.apply {
+                    beginTransaction()
+                            .add(R.id.container, HistoryFragment.newInstance())
+                            .addToBackStack("")
+                            .commitAllowingStateLoss()
+                }
+                true
+            }
+            R.id.menu_content_provider -> {
+                supportFragmentManager.apply{
+                    beginTransaction()
+                            .add(R.id.container, ContentProviderFragment.newInstance())
+                            .addToBackStack("")
+                            .commitAllowingStateLoss()
+                }
+                true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+
+    override fun onDestroy() {
+        unregisterReceiver(receiver)
+        super.onDestroy()
     }
 }
